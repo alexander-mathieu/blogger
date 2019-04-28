496b74569ecb60de22d5d1f304b423eb499181c7
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-04-28 17:52:21 -0600

    Add flash message to delete action with passing test

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     @article.save
 
-    flash.notice = "Article '#{@article.title}' was created.'"
+    flash.notice = "Article '#{@article.title}' was created."
 
     redirect_to article_path(@article)
   end
@@ -27,13 +27,15 @@ class ArticlesController < ApplicationController
   def update
     @article.update(article_params)
 
-    flash.notice = "Article '#{@article.title}' was updated.'"
+    flash.notice = "Article '#{@article.title}' was updated."
 
     redirect_to article_path(@article)
   end
 
   def destroy
     Article.destroy(params[:id])
+
+    flash.notice = "Article was deleted."
 
     redirect_to articles_path
   end
