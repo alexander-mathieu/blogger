a4262deaf72edb6c54f925699e1418854f888748
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-04-28 16:27:12 -0600

    Add delete article feature spec

---
+++ spec/features/user_deletes_an_article_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "user deletes an article" do
+  describe "they link from the show page" do
+    it "displays all articles without the deleted entry" do
+      article_1 = Article.create!(title: "Title 1", body: "Body 1")
+      article_2 = Article.create!(title: "Title 2", body: "Body 2")
+
+      visit article_path(article_1)
+      click_on "Delete"
+
+      expect(current_path).to eq(article_path)
+      expect(current_path).to have_content(article_2.title)
+      expect(current_path).to not_have_content(article_1.title)
+    end
+  end
+end
